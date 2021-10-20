6a05ee62cc877f75f4a086ea5e23b191b722fe4c
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2021-10-20 15:48:06 +0300

    fix classes

--- src/components/MatrixForm/MatrixForm.tsx
+++ src/components/MatrixForm/MatrixForm.tsx
@@ -3,22 +3,22 @@ import s from './MatrixForm.module.scss';
 
 const MatrixForm: FC = () => {
 	return (
-		<div className={s.matrix_form}>
-			<h2 className={s.matrix_form__title}>Matrix builder</h2>
-			<div className={s.matrix_form__block}>
-				<span className={s.matrix_form__text}>Enter the number of columns</span>
-				<input className={s.matrix_form__first_input} type="number" /> <br />
+		<div className={s.matrixForm}>
+			<h2 className={s.matrixForm__title}>Matrix builder</h2>
+			<div className={s.matrixForm__block}>
+				<span className={s.matrixForm__text}>Enter the number of columns</span>
+				<input className={s.matrixForm__input} type="number" />
 			</div>
-			<div className={s.matrix_form__block}>
-				<span className={s.matrix_form__text}>Enter the number of rows</span>
-				<input className={s.matrix_form__input} type="number" /> <br />
+			<div className={s.matrixForm__block}>
+				<span className={s.matrixForm__text}>Enter the number of rows</span>
+				<input className={s.matrixForm__input} type="number" />
 			</div>
-			<div className={s.matrix_form__block}>
-				<span className={s.matrix_form__text}>Enter the number of cells</span>
-				<input className={s.matrix_form__input} type="number" /> <br />
+			<div className={s.matrixForm__block}>
+				<span className={s.matrixForm__text}>Enter the number of cells</span>
+				<input className={s.matrixForm__input} type="number" />
 			</div>
 
-			<button className={s.matrix_form__button} type="button">
+			<button className={s.matrixForm__button} type="button">
 				Create matrix
 			</button>
 		</div>
