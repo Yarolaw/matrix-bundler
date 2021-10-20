27edc65087e77620d4be6d8f290c136a34a73bb3
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2021-10-20 11:55:56 +0300

    finish markup-form

--- src/components/MatrixForm.tsx
+++ src/components/MatrixForm.tsx
@@ -5,13 +5,22 @@ const MatrixForm: FC = () => {
 	return (
 		<div className={s.matrixForm}>
 			<h2 className={s.title}>Matrix builder</h2>
-			<span className={s.formText}>Enter the number of columns</span>
-			<input className={s.inputNumber} type="number" /> <br />
-			<span className={s.formText}>Enter the number of rows</span>
-			<input className={s.inputNumber} type="number" /> <br />
-			<span className={s.formText}>Enter the number of cells</span>
-			<input className={s.inputNumber} type="number" /> <br />
-			<button type="button">Create matrix</button>
+			<div className={s.formTextBlock}>
+				<span className={s.formText}>Enter the number of columns</span>
+				<input className={s.firstInputNumber} type="number" /> <br />
+			</div>
+			<div className={s.formTextBlock}>
+				<span className={s.formText}>Enter the number of rows</span>
+				<input className={s.inputNumber} type="number" /> <br />
+			</div>
+			<div className={s.formTextBlock}>
+				<span className={s.formText}>Enter the number of cells</span>
+				<input className={s.inputNumber} type="number" /> <br />
+			</div>
+
+			<button className={s.createMatrixBtn} type="button">
+				Create matrix
+			</button>
 		</div>
 	);
 };
